89fe5d86b593cb3808a170e1a66dcf2f7194723f
Hamza Zafar <link2hamzazafar@gmail.com>
2024-05-20 23:31:39 +0500

    added banner section to the website

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import Banner from "./Components/Banner";
 import Navbar from "./Components/Navbar";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Banner />
     </>
   )
 }
