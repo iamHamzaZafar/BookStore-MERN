feb4a0dbc6b713745552237c74e96d4525a080b0
Hamza Zafar <link2hamzazafar@gmail.com>
2024-05-25 17:08:09 +0500

    defined controller

---
+++ backend/Controller/book.controller.js
@@ -0,0 +1,11 @@
+import { Book } from "../Models/book.model.js";
+
+export const getBooks = async (req, res) => {
+  try {
+    const book = await Book.find();
+    res.status(200).json(book);
+  } catch (error) {
+    console.log("Error",error) 
+    res.status(500).json(error);
+  }
+};
